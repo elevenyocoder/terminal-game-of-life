fde301e7c847aafd23d2d676c82dcfd8074ad6df
elevenyocoder <59260554+elevenyocoder@users.noreply.github.com>
2020-01-01 19:01:37 +0100

    update cell placement main.c
    update cell placement main.c

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 #include <windows.h>
 #define I 30
 #define J 30
-#define T 50
+#define T 1
+#define TG 500
 int main(){
   
 	int map[I][J];
@@ -12,8 +13,11 @@ int main(){
 	int j = 0;
 	int x = 15;
 	int y = 15;
+  bool go = true;
+  bool put = false;
 	bool b = true;
   bool print = true;
+
 	char control = 0;
 
   system("cls");
@@ -24,7 +28,7 @@ int main(){
 			map[i][j] = 0;
 		}
 	}
-while(1){
+while(go){
       Sleep(T);
       if (kbhit())
       {
@@ -54,18 +58,34 @@ while(1){
                    	x++;
                   }        
                   print = true;
-                  break;                    
+                  break;      
+              case 99: // put cellule (C key). 
+                  map[x][y] = 2;        
+                  put = true;                     
+                  print = true;                  
+                  break;
+
+              case 32: // run simulation (space key).
+                  go = false;
+                  print = true;                  
+                  break;  
+
               default:
                 	break;                   
             }
         }
-        map[x][y] = 1;
-        
+
         if(print == true){
+          if(put == true){
+            map[x][y] = 2;
+          }     
+          else{
+            map[x][y] = 1;
+          }
           system("cls");
           for(i = 0;i < I;i++){
 			       for(j = 0;j < J;j++){
-				        if(map[j][i] == 1){                 
+				        if(map[j][i] == 1 ||map[j][i] == 2){                 
 				 	        printf("\033[1;32m");
                   printf("@");
                   printf("\033[0;35m"); 
@@ -75,16 +95,23 @@ while(1){
                   printf("\033[0;35m");         
 					        printf("_|");                 
 				        }
-			       }
-			
+			       }			
 			     printf("\n");
-        }
-  print = false;
-		    }
-	map[x][y] = 0;
-  print = false;
+        } 
 
+		    }
+  if(map[x][y] == 1){
+    map[x][y]=0;
+  }       	 
+  print = false;
+  put = false;  
     }
+  while(1){
+    Sleep(TG);
+    printf("\nnot rulesh");
+
+
+  }
 
 	return 0;
 }
