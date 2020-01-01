a4ba9e59f2927c06578643c318a056d05b7da819
elevenyocoder <59260554+elevenyocoder@users.noreply.github.com>
2020-01-01 20:27:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -13,6 +13,8 @@ int main(){
 	int j = 0;
 	int x = 15;
 	int y = 15;
+  int gen = 0 ;
+  int somme = 0;
   bool go = true;
   bool put = false;
 	bool b = true;
@@ -102,16 +104,95 @@ while(go){
 		    }
   if(map[x][y] == 1){
     map[x][y]=0;
-  }       	 
-  print = false;
-  put = false;  
-    }
-  while(1){
-    Sleep(TG);
-    printf("\nnot rulesh");
 
+  }       	 
+  
+  print = false;
+  put = false;
 
   }
 
+
+  while(1){
+    gen ++;
+    Sleep(TG);
+
+    for(i = 0;i < I;i++){
+        for(j = 0;j < J;j++){
+          somme = 0;                
+            if(j-1 >= 0 && j-1 < J && i-1 >= 0 && i-1 < I ){
+              if(map[j-1][i-1] == 2){
+                somme ++; 
+              }
+            }
+            if(i-1 >= 0 && i-1 < I ){
+              if(map[j][i-1] == 2){
+                somme ++; 
+              }
+            }
+            if(j+1 >= 0 && j+1 < J && i-1 >= 0 && i-1 < I ){
+              if(map[j+1][i-1] == 2){
+                somme ++; 
+              }
+            }
+            if(j-1 >= 0 && j-1 < J){
+              if(map[j-1][i] == 2){
+                somme ++; 
+              }
+            }
+            if(j+1 >= 0 && j+1 < J){
+              if(map[j+1][i] == 2){
+                somme ++; 
+              }
+            }
+            if(j-1 >= 0 && j-1 < J && i+1 >= 0 && i+1 < I ){
+              if(map[j-1][i+1] == 2){
+                somme ++; 
+              }
+            }
+            if(i+1 >= 0 && i+1 < I){
+              if(map[j][i+1] == 2){
+                somme ++; 
+              }
+            }
+            if(j+1 >= 0 && j+1 < J && i+1 >= 0 && i+1 < I ){
+              if(map[j+1][i+1] == 2){
+                somme ++; 
+              }
+            }
+
+            if(map[j][i] == 2){
+              if(somme != 2 && somme != 3){
+                map[j][i] = 0;
+              }
+            }
+            else{
+              if(somme == 3){
+                map[j][i] == 2;
+              }
+            }
+        }             
+    }
+    system("cls");
+    for(i = 0;i < I;i++){
+        for(j = 0;j < J;j++){
+          if(map[j][i] == 2){                 
+             printf("\033[1;32m");
+             printf("@");
+             printf("\033[0;35m"); 
+             printf("|");
+            }
+          else{    
+            printf("\033[0;35m");         
+            printf("_|");                 
+          }
+        }      
+      printf("\n");
+    }
+    printf("\033[1;32m");
+    printf("GENERATION : %d",gen);
+    printf("\033[0m");  
+  }
 	return 0;
 }
+
